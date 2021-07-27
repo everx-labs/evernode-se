9298c0678f608a47a0ffca70d6fb741eca02a7dc
Denis K <@tonlabs.io>
2021-07-27 19:22:07 +0300

    Fixed building by removing ADNL, which is useless for Node SE.

--- ton-node-se/adnl/src/debug.rs
+++
@@ -1,9 +0,0 @@
-pub const TARGET: &str = "adnl";
-
-pub fn dump(m: &str, d: &[u8]) {
-    let mut dump = String::new();
-    for i in 0..d.len() {
-        dump.push_str(&format!("{:02x}{}", d[i], if (i + 1) % 16 == 0 { '\n' } else { ' ' }));
-    }
-    debug!(target: TARGET, "{}:\n{}", m, dump);
-}
