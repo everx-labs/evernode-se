cb4fcdb51337f2dd83684244e8e6c8b1d49c2f8e
Alexey Vavilin <mnogoborec@mail.ru>
2023-02-17 13:17:13 +0300

    Fix GiverV3 replay proitection to check body hash

--- contracts/giver_v3/GiverV3.sol
+++ contracts/giver_v3/GiverV3.sol
@@ -55,14 +55,14 @@ contract GiverV3 is Upgradable {
     {
         // owner check
         require(msg.pubkey() == tvm.pubkey(), 101);
+        uint256 bodyHash = tvm.hash(body);
         // load and drop message timestamp (uint64)
         (, uint32 expireAt) = body.decode(uint64, uint32);
         require(expireAt > now, 57);
-        uint256 msgHash = tvm.hash(message);
-        require(!m_messages.exists(msgHash), 102);
+        require(!m_messages.exists(bodyHash), 102);
 
         tvm.accept();
-        m_messages[msgHash] = expireAt;
+        m_messages[bodyHash] = expireAt;
 
         return body;
     }
@@ -70,13 +70,13 @@ contract GiverV3 is Upgradable {
     /// @notice Allows to delete expired messages from dict.
     function gc() private inline {
         uint counter = 0;
-        for ((uint256 msgHash, uint32 expireAt) : m_messages) {
+        for ((uint256 bodyHash, uint32 expireAt) : m_messages) {
             if (counter >= MAX_CLEANUP_MSGS) {
                 break;
             }
             counter++;
             if (expireAt <= now) {
-                delete m_messages[msgHash];
+                delete m_messages[bodyHash];
             }
         }
     }
